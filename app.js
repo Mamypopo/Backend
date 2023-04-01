99543aadcb7abe4fc9d62eb9fd50264e50ca826a
Mamypopo <97841467+Mamypopo@users.noreply.github.com>
2023-04-02 04:03:29 +0700

    up

--- app.js
+++ app.js
@@ -38,20 +38,59 @@ app.post('/signup', jsonParser, function (req, res, next) {
     
 })
 
-
+app.post('/signupteachers', jsonParser, function (req, res, next) {
+    bcrypt.hash(req.body.tpassword, saltRounds, function(err, hash) {
+        connection.execute(
+            
+            'INSERT INTO teachers (temail, tpassword, tfname, tlname, tphone, tfaculty, tbranch) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [req.body.temail,hash, req.body.tfname, req.body.tlname, req.body.tphone, req.body.tfaculty, req.body.tbranch],
+            function(err, results, fields) {
+             if(err){
+                res.json({status:'error',massage: err})
+                return
+             } 
+             res.json({status:'ok'})
+            }
+        );
+    });
+    
+    
+})
 
 app.post('/signin', jsonParser, function (req, res, next) {
     connection.execute(
         'SELECT * FROM users WHERE email=?',
         [req.body.email],
-        function(err, users, fields) {
+        function(err, users, fields, ) {
             if(err){res.json({status:'error',massage: err}); return  }
             if (users.length ==0) {res.json({status:'error',massage: 'no user found'}); return  }
-            console.log(users);
             if (users[0].role == 'admin'){res.json({status:'error',massage: err}); return  }
             bcrypt.compare(req.body.password, users[0].password, function(err, isLogin) {
                 if (isLogin){
                     var token = jwt.sign({ email: users[0].email }, secret ,{ expiresIn: '1h' });
+                    res.json({status:'ok', massage:'login success',token })
+                } else {
+                    res.json({status:'errrr', massage:'login failed'})
+                }
+                
+            });
+          
+        }
+    );
+})
+
+app.post('/signinteachers', jsonParser, function (req, res, next) {
+    connection.execute(
+        'SELECT * FROM teachers WHERE temail=?',
+        [req.body.temail],
+        function(err, teachers, fields, ) {
+            if(err){res.json({status:'error',massage: err}); return  }
+            console.log(teachers);
+            if (teachers.length ==0) {res.json({status:'error',massage: 'no user found'}); return  }
+            //if (teachers[0].role == 'admin'){res.json({status:'error',massage: err}); return  }
+            bcrypt.compare(req.body.tpassword, teachers[0].tpassword, function(err, isLoginteachers) {
+                if (isLoginteachers){
+                    var token = jwt.sign({ temail: teachers[0].temail }, secret ,{ expiresIn: '1h' });
                     res.json({status:'ok', massage:'login success',token })
                 } else {
                     res.json({status:'errrr', massage:'login failed'})
@@ -99,6 +138,17 @@ app.post('/authen', jsonParser, function (req, res, next) {
     
 })
 
+app.post('/authenteachers', jsonParser, function (req, res, next) {
+    try
+    {
+        const token = req.headers.authorization.split(' ')[1]
+         var decoded = jwt.verify(token, secret);
+         res.json({status:'ok',decoded })
+        } catch(err) {
+        res.json({status:'error', massage : err.massage})
+    }
+    
+})
 app.listen(3333,  function () {
   console.log('CORS-enabled web server listening on port 3333')
 })
